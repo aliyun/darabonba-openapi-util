5d81eccb0fd052d5de67b206f9363e69f220a0c4
普冬 <cpj1106@gmail.com>
2021-01-07 12:30:11 +0800

    add java sm3 sign

--- java/src/main/java/com/aliyun/openapiutil/Client.java
+++ java/src/main/java/com/aliyun/openapiutil/Client.java
@@ -4,14 +4,24 @@ package com.aliyun.openapiutil;
 import com.aliyun.tea.*;
 import com.aliyun.tea.utils.StringUtils;
 import com.google.gson.Gson;
+import org.bouncycastle.crypto.digests.SM3Digest;
+import org.bouncycastle.crypto.macs.HMac;
+import org.bouncycastle.crypto.params.KeyParameter;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 import javax.crypto.Mac;
+import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import javax.xml.bind.DatatypeConverter;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Field;
 import java.net.URLEncoder;
+import java.security.KeyFactory;
+import java.security.MessageDigest;
+import java.security.PrivateKey;
+import java.security.Signature;
+import java.security.spec.PKCS8EncodedKeySpec;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
@@ -21,7 +31,15 @@ public class Client {
     public final static String SEPARATOR = "&";
     public final static String URL_ENCODING = "UTF-8";
     public static final String ALGORITHM_NAME = "HmacSHA1";
-
+    public static final String HASH_SHA256 = "SHA-256";
+    public static final String HASH_SM3 = "SM3";
+    public static final String HMAC_SHA256 = "ACS3-HMAC-SHA256";
+    public static final String RSA_SHA256 = "ACS3-RSA-SHA256";
+    public static final String HMAC_SM3 = "ACS3-HMAC-SM3";
+    public static final String UTF8 = "UTF-8";
+    public static final String PEM_BEGIN = "-----BEGIN RSA PRIVATE KEY-----\n";
+    public static final String PEM_END = "\n-----END RSA PRIVATE KEY-----";
+;
     /**
      * Convert all params of body other than type of readable into content
      *
@@ -98,27 +116,80 @@ public class Client {
         return result.toString();
     }
 
+    protected static Map<String, String> getCanonicalizedHeadersMap(Map<String, String> headers) {
+        String prefix = "x-acs-";
+        Set<String> keys = headers.keySet();
+        List<String> canonicalizedKeys = new ArrayList<>();
+        Map<String, List<String>> valueMap = new HashMap<>();
+        for (String key : keys) {
+            String lowerKey = key.toLowerCase();
+            if (lowerKey.startsWith(prefix) || lowerKey.equals("host")
+                    || lowerKey.equals("content-type")) {
+                List<String> valueList;
+                if(valueMap.containsKey(lowerKey)){
+                    valueList = valueMap.get(lowerKey);
+                }else {
+                    canonicalizedKeys.add(lowerKey);
+                    valueList = new ArrayList<>();
+                }
+                valueList.add(headers.get(key).trim());
+                valueMap.put(lowerKey, valueList);
+            }
+        }
+        String[] canonicalizedKeysArray = canonicalizedKeys.toArray(new String[canonicalizedKeys.size()]);
+        Map<String, String> result = new HashMap<>();
+        String signedHeaders = StringUtils.join(";", Arrays.asList(canonicalizedKeysArray));
+        Arrays.sort(canonicalizedKeysArray);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < canonicalizedKeysArray.length; i++) {
+            String key = canonicalizedKeysArray[i];
+            List<String> valueList = valueMap.get(key);
+            String[] valueArray = valueList.toArray(new String[valueList.size()]);
+            Arrays.sort(valueArray);
+            sb.append(key);
+            sb.append(":");
+            sb.append(StringUtils.join(",", Arrays.asList(valueArray)));
+            sb.append("\n");
+        }
+        result.put("canonicalHeaders", sb.toString());
+        result.put("signedHeaders", signedHeaders);
+        return result;
+    }
+
+    protected static String getCanonicalizedQueryString(StringBuilder sb, Map<String, String> query, String[] keys){
+        Arrays.sort(keys);
+        String key;
+        String value;
+        for (int i = 0; i < keys.length; i++) {
+            key = keys[i];
+            sb.append(key);
+            value = query.get(key);
+            if (!StringUtils.isEmpty(value) && !"".equals(value.trim())) {
+                sb.append("=");
+                sb.append(value);
+            }
+            sb.append("&");
+        }
+        return sb.deleteCharAt(sb.length() - 1).toString();
+    }
+
+    protected static String getCanonicalizedResource(Map<String, String> query) {
+        String[] keys = query.keySet().toArray(new String[query.size()]);
+        if (keys.length <= 0) {
+            return "";
+        }
+        StringBuilder result = new StringBuilder();
+        return getCanonicalizedQueryString(result, query, keys);
+    }
+
     protected static String getCanonicalizedResource(String pathname, Map<String, String> query) {
         String[] keys = query.keySet().toArray(new String[query.size()]);
         if (keys.length <= 0) {
             return pathname;
         }
-        Arrays.sort(keys);
         StringBuilder result = new StringBuilder(pathname);
         result.append("?");
-        String key;
-        String value;
-        for (int i = 0; i < keys.length; i++) {
-            key = keys[i];
-            result.append(key);
-            value = query.get(key);
-            if (!StringUtils.isEmpty(value) && !"".equals(value.trim())) {
-                result.append("=");
-                result.append(value);
-            }
-            result.append("&");
-        }
-        return result.deleteCharAt(result.length() - 1).toString();
+        return getCanonicalizedQueryString(result, query, keys);
     }
 
     /**
@@ -133,8 +204,8 @@ public class Client {
             return secret;
         }
         Mac mac = Mac.getInstance("HmacSHA1");
-        mac.init(new SecretKeySpec(secret.getBytes("UTF-8"), "HmacSHA1"));
-        byte[] signData = mac.doFinal(stringToSign.getBytes("UTF-8"));
+        mac.init(new SecretKeySpec(secret.getBytes(UTF8), "HmacSHA1"));
+        byte[] signData = mac.doFinal(stringToSign.getBytes(UTF8));
         return Base64.getEncoder().encodeToString(signData);
     }
 
@@ -161,9 +232,9 @@ public class Client {
             } else {
                 result.append(symbol);
             }
-            result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+            result.append(URLEncoder.encode(entry.getKey(), UTF8));
             result.append("=");
-            result.append(URLEncoder.encode(String.valueOf(entry.getValue()), "UTF-8"));
+            result.append(URLEncoder.encode(String.valueOf(entry.getValue()), UTF8));
         }
         return result.toString();
     }
@@ -212,7 +283,7 @@ public class Client {
                 key = key.substring(1);
             }
             if (value instanceof byte[]) {
-                map.put(key, new String((byte[]) value, "UTF-8"));
+                map.put(key, new String((byte[]) value, UTF8));
             } else {
                 map.put(key, String.valueOf(value));
             }
@@ -339,5 +410,114 @@ public class Client {
             return "oss-accelerate.aliyuncs.com";
         }
         return endpoint;
+    }
+
+    public static String hexEncode(byte[] raw){
+        if(raw == null){
+            return "";
+        }
+        StringBuffer sb = new StringBuffer();
+        for(int i = 0; i < raw.length; i++) {
+            String hex = Integer.toHexString(raw[i] & 0xFF);
+            if(hex.length() < 2){
+                sb.append(0);
+            }
+            sb.append(hex);
+        }
+        return sb.toString();
+    }
+
+    public static byte[] hash(byte[] raw, String signAlgorithm) throws Exception{
+        if(signAlgorithm == null){
+            return null;
+        }
+        if(signAlgorithm.equals(HMAC_SHA256) || signAlgorithm.equals(RSA_SHA256)){
+            MessageDigest digest = MessageDigest.getInstance(HASH_SHA256);
+            return digest.digest(raw);
+        } else if(signAlgorithm.equals(HMAC_SM3)){
+            BouncyCastleProvider provider = new BouncyCastleProvider();
+            MessageDigest digest = MessageDigest.getInstance(HASH_SM3, provider);
+            return digest.digest(raw);
+        }
+        return null;
+    }
+
+
+
+
+    public static String getAuthorization(TeaRequest request, String signAlgorithm, String payload, String accessKey, String secret)throws Exception{
+        String canonicalURI = percentEncode(request.pathname);
+        if(canonicalURI == null){
+            canonicalURI = "";
+        }
+        String method = request.method;
+        Map<String, String> headers = request.headers;
+        Map<String, String> query = request.query;
+        Map<String, String> cannoicalHeaders = getCanonicalizedHeadersMap(headers);
+        String signedHeaders = cannoicalHeaders.get("signedHeaders");
+        String queryString = getCanonicalizedResource(query);
+        StringBuilder sb = new StringBuilder(method);
+        sb.append("\n").append(canonicalURI).append("\n").append(queryString).append("\n").append(cannoicalHeaders.get("canonicalHeaders"))
+                .append("\n").append(signedHeaders).append("\n").append(payload);
+        String hex = hexEncode(hash(sb.toString().getBytes(UTF8), signAlgorithm));
+        String stringToSign = signAlgorithm + "\n" + hex;
+        String signature = hexEncode(SignatureMethod(stringToSign, secret, signAlgorithm));
+        String auth = signAlgorithm + " Credential=" + accessKey + ",SignedHeaders=" + signedHeaders
+                + ",Signature=" + signature;
+        return auth;
+    }
+
+    protected static String checkRSASecret(String secret){
+        if(secret.startsWith(PEM_BEGIN)){
+            secret = secret.replace(PEM_BEGIN, "");
+        }
+
+        while(secret.endsWith("\n") || secret.endsWith("\r")){
+            secret = secret.substring(0, secret.length() - 1);
+        }
+
+        if(secret.endsWith(PEM_END)){
+            secret = secret.replace(PEM_END, "");
+        }
+
+        return secret;
+    }
+
+    public static byte[] SignatureMethod(String stringToSign, String secret, String signAlgorithm) throws Exception {
+        byte[] bytes = null;
+        if(signAlgorithm.equals(HMAC_SHA256)){
+            Mac sha256_HMAC = Mac.getInstance("HmacSHA256");
+            SecretKeySpec secret_key = new SecretKeySpec(secret.getBytes(), "HmacSHA256");
+            sha256_HMAC.init(secret_key);
+            bytes = sha256_HMAC.doFinal(stringToSign.getBytes());
+            return bytes;
+        } else if(signAlgorithm.equals(RSA_SHA256)){
+            secret = checkRSASecret(secret);
+            Signature rsaSign = Signature.getInstance("SHA256withRSA");
+            KeyFactory kf = KeyFactory.getInstance("RSA");
+            byte[] keySpec = DatatypeConverter.parseBase64Binary(secret);
+            PrivateKey privateKey = kf.generatePrivate(new PKCS8EncodedKeySpec(keySpec));
+            rsaSign.initSign(privateKey);
+            rsaSign.update(stringToSign.getBytes(UTF8));
+            bytes = rsaSign.sign();
+        }else if(signAlgorithm.equals(HMAC_SM3)){
+            SecretKey key = new SecretKeySpec((secret).getBytes(UTF8), "HMAC-SM3");
+            HMac mac = new HMac(new SM3Digest());
+            bytes = new byte[mac.getMacSize()];
+            byte[] inputBytes = stringToSign.getBytes(UTF8);
+            mac.init(new KeyParameter(key.getEncoded()));
+            mac.update(inputBytes, 0, inputBytes.length);
+            mac.doFinal(bytes, 0);
+        }
+        return bytes;
+    }
+
+    public static String getEndpointPath(String path) throws UnsupportedEncodingException {
+        String[] strs = path.split("/");
+        for(int i = 0; i < strs.length; i++) {
+            strs[i] = percentEncode(strs[i]);
+        }
+        List<String> strList = Arrays.asList(strs);
+        return String.join("/", strList);
     }
 }
