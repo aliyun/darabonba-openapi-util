a1cd5584cc3d4b2671b034d15fd6d1b2b0105c7b
普冬 <cpj1106@gmail.com>
2021-01-07 12:30:11 +0800

    add java sm3 sign

--- java/src/test/java/com/aliyun/openapiutil/ClientTest.java
+++ java/src/test/java/com/aliyun/openapiutil/ClientTest.java
@@ -242,4 +242,74 @@ public class ClientTest {
         Assert.assertEquals("test",
                 Client.getEndpoint("test", true, "test"));
     }
+
+    @Test
+    public void hexEncodeTest() throws Exception {
+        byte[] hash = Client.hash("test".getBytes(Client.UTF8), "ACS3-HMAC-SHA256");
+        String result = Client.hexEncode(hash);
+        Assert.assertEquals("9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08", result);
+
+        hash = Client.hash("test".getBytes(Client.UTF8), "ACS3-RSA-SHA256");
+        result = Client.hexEncode(hash);
+        Assert.assertEquals("9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08", result);
+
+        hash = Client.hash("test".getBytes(Client.UTF8), "ACS3-HMAC-SM3");
+        result = Client.hexEncode(hash);
+        Assert.assertEquals("55e12e91650d2fec56ec74e1d3e4ddbfce2ef3a65890c2a19ecf88a307e76a23", result);
+    }
+
+    @Test
+    public void getEndpointPathTest() throws Exception {
+        String result = Client.getEndpointPath("/path/ test");
+        Assert.assertEquals("/path/%20test", result);
+        result = Client.getEndpointPath("/path/#test");
+        Assert.assertEquals("/path/%23test", result);
+        result = Client.getEndpointPath("/path/\"test");
+        Assert.assertEquals("/path/%22test", result);
+    }
+
+    @Test
+    public void getAuthorizationTest() throws Exception {
+        Map<String, String> query = new HashMap<>();
+        query.put("test", "ok");
+        query.put("empty", "");
+
+        Map<String, String> headers = new HashMap<>();
+        headers.put("x-acs-test", "http");
+        headers.put("x-acs-TEST", "https");
+
+        TeaRequest req = new TeaRequest();
+        req.query = query;
+        req.headers = headers;
+
+        String auth = Client.getAuthorization(req, "ACS3-HMAC-SHA256",
+                "55e12e91650d2fec56ec74e1d3e4ddbfce2ef3a65890c2a19ecf88a307e76a23","acesskey", "secret");
+        Assert.assertEquals("ACS3-HMAC-SHA256 Credential=acesskey,SignedHeaders=x-acs-test,Signature=da772425f29289d3460d5fc961455d40c5e8c6afd0888b78a910c991e6a14846", auth);
+    }
+
+    @Test
+    public void signatureMethodTest() throws Exception {
+
+        String priKey = "-----BEGIN RSA PRIVATE KEY-----\nMIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAKzSQmrnH0YnezZ9" +
+        "8NK50WjMuci0hgGVcSthIZOTWMIySznY9Jj1hlvek7W0uYagtFHz03BHQnHAb5Xs" +
+        "0DZm0Sj9+5r79GggwEzTJDYEsLyFwXM3ZOIxqxL4sRg94MHsa81M9NXGHMyMvvff" +
+        "QTn1OBVLTVz5jgJ48foMn7j7r9kRAgMBAAECgYEAnZppw3/ef2XF8Z3Mnv+iP0Zk" +
+        "LuqiQpN8TykXK7P1/7NJ8wktlshhrSo/3jdf8axghVQsgHob2Ay8Nidugg4lsxIL" +
+        "AUBHvfQsQp1MAWvxslsVj+ddw01MQnt8kHmC/qhok+YuNqqAGBcoD6cthRUjEri6" +
+        "hfs599EfPs2DcWW06qECQQDfNqUUhcDQ/SQHRhfY9UIlaSEs2CVagDrSYFG1wyG+" +
+        "PXDSMes9ZRHsvVVBmNGmtUTg/jioTU3yuPsis5s9ppbVAkEAxjTAQxv5lBBm/ikM" +
+        "TzPShljxDZnXh6lKWG9gR1p5fKoQTzLyyhHzkBSFe848sMm68HWCX2wgIpQLHj0G" +
+        "ccYPTQJAduMKBeY/jpBlkiI5LWtj8b0O2G2/Z3aI3ehDXQYzgLoEz0+bNbYRWAB3" +
+        "2lpkv+AocZW1455Y+ACichcrhiimiQJAW/6L5hoL4u8h/oFq1zAEXJrXdyqaYLrw" +
+        "aM947mVN0dDVNQ0+pw9h7tO3iNkWTi+zdnv0APociDASYPyOCyyUWQJACMNRM1/r" +
+        "boXuKfMmVjmmz0XhaDUC/JkqSwIiaZi+47M21e9BTp1218NA6VaPgJJHeJr4sNOn" +
+        "Ysx+1cwXO5cuZg==-----END RSA PRIVATE KEY-----\n\r\r";
+
+        byte[] signature = Client.SignatureMethod("source", "secret", "ACS3-HMAC-SM3");
+        Assert.assertEquals("b9ff646822f41ef647c1416fa2b8408923828abc0464af6706e18db3e8553da8", Client.hexEncode(signature));
+
+        signature = Client.SignatureMethod("source", priKey, "ACS3-RSA-SHA256");
+        Assert.assertEquals("a00b88ae04f651a8ab645e724949ff435bbb2cf9a37aa54323024477f8031f4e13dc948484c5c5a81ba53a55eb0571dffccc1e953c93269d6da23ed319e0f1ef699bcc9823a646574628ae1b70ed569b5a07d139dda28996b5b9231f5ba96141f0893deec2fbf54a0fa2c203b8ae74dd26f457ac29c873745a5b88273d2b3d12", Client.hexEncode(signature));
+    }
+
 }
