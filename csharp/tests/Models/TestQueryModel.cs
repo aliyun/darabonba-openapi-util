997916d1218deb16c2dc2526eb3bf997b6d58fb2
wen ping <1425499266@qq.com>
2022-04-26 15:59:02 +0800

    优化C# query中teamodel结构解析问题

---
+++ csharp/tests/Models/TestQueryModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tea;
+
+namespace tests.Models
+{
+    public class TestQueryModel : TeaModel
+    {
+        public class TestQueryModelItems : TeaModel
+        {
+            [NameInMap("Description")]
+            [Validation(Required = false)]
+            public string Description
+            {
+                get;
+                set;
+            }
+
+            [NameInMap("ItemId")]
+            [Validation(Required = false)]
+            public int? ItemId
+            {
+                get;
+                set;
+            }
+
+        }
+
+        [NameInMap("ItemGroupId")]
+        [Validation(Required = false)]
+        public string ServerGroupId
+        {
+            get;
+            set;
+        }
+
+        [NameInMap("Items")]
+        [Validation(Required = false)]
+        public List<TestQueryModelItems> Items
+        {
+            get;
+            set;
+        }
+    }
+}
