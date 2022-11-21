a8b5acf04af7859d81840d028d03fffa8da586b4
yndu13 <yndu13@gmail.com>
2022-11-21 17:44:22 +0800

    [csharp]resolve stream deeply in convert

--- csharp/tests/Models/TestConvertModel.cs
+++ csharp/tests/Models/TestConvertModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 
 using Tea;
 
@@ -6,19 +7,38 @@ namespace tests.Models
 {
     public class TestConvertModel : TeaModel
     {
+        [NameInMap("RequestId")]
         public string RequestId { get; set; }
 
+        [NameInMap("NoMap1")]
         public int NoMap { get; set; }
 
+        [NameInMap("Dict")]
         public Dictionary<string, object> Dict { get; set; }
 
+        [NameInMap("SubModel")]
         public TestConvertSubModel SubModel { get; set; }
+
+        [NameInMap("Url")]
+        public Stream UrlObject { get; set; }
+
+        [NameInMap("List")]
+        public List<Stream> ListObject { get; set; }
+
+        [NameInMap("UrlList")]
+        public List<UrlList> UrlListObject { get; set; }
 
         public class TestConvertSubModel : TeaModel
         {
             public string RequestId { get; set; }
 
             public int Id { get; set; }
+        }
+
+        public class UrlList : TeaModel
+        {
+            [NameInMap("Url")]
+            public Stream UrlObject { get; set; }
         }
     }
 }
